c390afb0ec5b6f14cff9898a8f51e5953bd54c79
coderhst <1727926855@qq.com>
2022-07-07 17:41:39 +0800

    master

---
+++ src/network/request.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export function request(config) {
+  //1. 创建axios实例
+  const instance = axios.create({
+    baseURL: "http://123.207.32.32:8000",
+    timeout: 5000
+  });
+
+  //2. axios拦截器
+  //2.1 axios请求拦截
+  instance.interceptors.request.use(
+    config => {
+      // 一定要返回
+      return config;
+    },
+    err => {
+      console.log(err);
+    }
+  );
+
+  // 2.2 axios响应拦截
+  instance.interceptors.response.use(
+    res => {
+      return res.data;
+    },
+    err => {
+      console.log(err);
+    }
+  );
+
+  // 3.发送真正的网络请求
+  return instance(config);
+}
